8757881bad584729d2f6213820d0f10a34926418
carlo697 <carlos.07888@gmail.com>
2023-07-10 16:13:15 -0400

    refactor: remove unused fields in the 'FalloffNoiseGenerator' class

--- Assets/Scripts/Terrain/Noise/FalloffNoiseGenerator.cs
+++ Assets/Scripts/Terrain/Noise/FalloffNoiseGenerator.cs
@@ -9,8 +9,6 @@ public class FalloffNoiseGenerator {
   public float lacunarity = 2f;
   public int octaves = 9;
 
-  public float seaBorderBeforeThreshold = 0.05f;
-  public float seaBorderAfterThreshold = 0.1f;
   public float landGap = 0.15f;
   public AnimationCurve falloffGradientCurve = AnimationCurve.Linear(-1f, -1f, 1f, 1f);
   public AnimationCurve falloffOutputCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
