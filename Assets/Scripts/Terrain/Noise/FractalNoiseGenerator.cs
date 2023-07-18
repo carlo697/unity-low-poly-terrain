478576c79eb59f7ae2f704b7a4774e166c36d140
carlo697 <carlos.07888@gmail.com>
2023-07-17 23:14:02 -0400

    feat: add fields to select the type of noise in the 'FractalNoiseGenerator' class

--- Assets/Scripts/Terrain/Noise/FractalNoiseGenerator.cs
+++ Assets/Scripts/Terrain/Noise/FractalNoiseGenerator.cs
@@ -6,15 +6,31 @@ public class FractalNoiseGenerator : TerrainNoiseGenerator {
   public bool is3d = false;
   public int seed = 0;
   public float scale = 1f;
+  public NoiseType noiseType = NoiseType.Simplex;
   public float gain = 0.5f;
   public float lacunarity = 2f;
   public int octaves = 1;
+  public FractalType fractalType = FractalType.FractalFBm;
   public bool useCurve = false;
   public AnimationCurve curve = AnimationCurve.Linear(-1f, -1f, 1f, 1f);
 
+  public enum FractalType {
+    [InspectorName("Fractal FBm")]
+    FractalFBm,
+    FractalRidged
+  };
+
+  public enum NoiseType {
+    Value,
+    Perlin,
+    Simplex,
+    OpenSimplex2,
+    OpenSimplex2S
+  };
+
   public float[] GenerateNoise(TerrainChunk chunk, float frequency, int seed) {
-    FastNoise noise = new FastNoise("FractalFBm");
-    noise.Set("Source", new FastNoise("Simplex"));
+    FastNoise noise = new FastNoise(fractalType.ToString());
+    noise.Set("Source", new FastNoise(noiseType.ToString()));
     noise.Set("Gain", gain);
     noise.Set("Lacunarity", lacunarity);
     noise.Set("Octaves", octaves);
