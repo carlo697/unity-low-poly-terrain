99422d992a42f98eb47b77124e7d98001d902079
carlo697 <carlos.07888@gmail.com>
2023-07-30 16:57:49 -0400

    feat: prevent saving meshes in the colliders of terrain chunks when saving scenes in the editor

--- Assets/Scripts/Terrain/Editor/PreventSavingTerrainChunkMeshes.cs
+++ Assets/Scripts/Terrain/Editor/PreventSavingTerrainChunkMeshes.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 [UnityEditor.InitializeOnLoad]
 static class PreventSavingTerrainChunkMeshes {
   private static Dictionary<MeshFilter, Mesh> cachedMeshes = new Dictionary<MeshFilter, Mesh>();
+  private static Dictionary<MeshCollider, Mesh> cachedColliders = new Dictionary<MeshCollider, Mesh>();
 
   static PreventSavingTerrainChunkMeshes() {
     EditorSceneManager.sceneSaved += OnSceneSaved;
@@ -23,11 +24,21 @@ static class PreventSavingTerrainChunkMeshes {
         cachedMeshes.Add(meshFilter, meshFilter.sharedMesh);
         meshFilter.sharedMesh = null;
       }
+
+      MeshCollider meshCollider = chunk.GetComponent<MeshCollider>();
+      if (meshCollider) {
+        cachedColliders.Add(meshCollider, meshCollider.sharedMesh);
+        meshCollider.sharedMesh = null;
+      }
     }
   }
 
   static void OnSceneSaved(Scene scene) {
     foreach (var item in cachedMeshes) {
+      item.Key.sharedMesh = item.Value;
+    }
+
+    foreach (var item in cachedColliders) {
       item.Key.sharedMesh = item.Value;
     }
   }
