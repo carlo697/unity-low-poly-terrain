497a3a3839481200b17b49812269dd28c4efd8b4
carlo697 <carlos.07888@gmail.com>
2024-01-10 23:00:59 -0400

    feat: add class to generate cellular noise with FastNoise2

---
+++ Assets/Scripts/Terrain/Noise/CellularNoiseGenerator.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Runtime.CompilerServices;
+
+[Serializable]
+public class CellularNoiseGenerator {
+  public int seed = 0;
+  public float scale = 1f;
+  public CellularNoiseType noiseType = CellularNoiseType.CellularValue;
+  public float jitterModifier = 1f;
+
+  public class Generator : TerrainNoiseGenerator {
+    private CellularNoiseGenerator m_settings;
+    private FastNoise m_noise;
+
+    public Generator(CellularNoiseGenerator settings) {
+      m_settings = settings;
+      m_noise = new FastNoise(settings.noiseType.ToString());
+      m_noise.Set("Jitter Modifier", settings.jitterModifier);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float Generate3d(float x, float y, float z, int seed) {
+      // Generate noise from a 2d point
+      float value = m_noise.GenSingle3D(
+        x / m_settings.scale,
+        y / m_settings.scale,
+        z / m_settings.scale,
+        m_settings.seed + seed
+      );
+
+      return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float Generate2d(float x, float y, int seed) {
+      // Generate noise from a 2d point
+      float value = m_noise.GenSingle2D(
+        x / m_settings.scale,
+        y / m_settings.scale,
+        m_settings.seed + seed
+      );
+
+      return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float[] GenerateGrid3d(FastNoiseChunk tile, float scale, int terrainSeed) {
+      return GenerateGrid(true, tile, scale, terrainSeed);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float[] GenerateGrid2d(FastNoiseChunk tile, float scale, int terrainSeed) {
+      return GenerateGrid(false, tile, scale, terrainSeed);
+    }
+
+    private float[] GenerateGrid(bool is3d, FastNoiseChunk tile, float scale, int terrainSeed) {
+      float[] pixels = tile.GenerateGrid(
+        is3d,
+        m_noise,
+        terrainSeed + m_settings.seed,
+        m_settings.scale * scale
+      );
+
+      return pixels;
+    }
+  }
+
+  public Generator GetGenerator() {
+    return new Generator(this);
+  }
+}
---
+++ Assets/Scripts/Terrain/Noise/CellularNoiseType.cs
@@ -0,0 +1,4 @@
+public enum CellularNoiseType {
+  CellularValue,
+  CellularDistance
+};
